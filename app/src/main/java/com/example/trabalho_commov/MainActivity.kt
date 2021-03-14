28a1f94b53d12924066c3ca000f6c790a6a62395
Hugo Pereira <61696448+EvocativeAxisPt@users.noreply.github.com>
2021-03-14 13:30:51 +0000

    buttons

--- app/src/main/java/com/example/trabalho_commov/MainActivity.kt
+++ app/src/main/java/com/example/trabalho_commov/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.trabalho_commov
 
 import Adapter.Adapter
+import android.graphics.Insets.add
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.codinginflow.recyclerviewexample.Nota
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.random.Random
+import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +24,19 @@ class MainActivity : AppCompatActivity() {
         recycler_view.setHasFixedSize(true)
 
     }
+
+
+    fun insertItem(view: View) {
+        val index = Random.nextInt(8)
+
+
+    }
+    fun removeItem(view: View) {
+
+        
+    }
+
+
     private fun generateDummyList(size: Int): List<Nota> {
         val list = ArrayList<Nota>()
         for (i in 0 until size) {
